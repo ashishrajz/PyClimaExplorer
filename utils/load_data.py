ad7fbc4418a42026bb495a883f815d0019ab0b81
Ashish Raj <ashishraj@Ashishs-MacBook-Air-M3.local>
2026-03-15 04:33:09 +0530

    fix dataset path for deployment

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,20 +1,26 @@
 import xarray as xr
 import streamlit as st
+import os
+
 
 @st.cache_data
 def load_dataset(path):
 
-    # If multiple files (daily wind)
-    if isinstance(path, list):
-        ds = xr.merge([xr.open_dataset(p) for p in path])
-    else:
-        ds = xr.open_dataset(path)
+    # Resolve absolute path (works on Streamlit Cloud)
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-    # Convert temperature if present
+    if isinstance(path, list):
+        files = [os.path.join(BASE_DIR, p) for p in path]
+        ds = xr.merge([xr.open_dataset(f) for f in files])
+    else:
+        file_path = os.path.join(BASE_DIR, path)
+        ds = xr.open_dataset(file_path)
+
+    # Convert temperature
     if "t2m" in ds:
         ds["t2m"] = ds["t2m"] - 273.15
 
-    # Create wind speed if wind components exist
+    # Wind speed
     if "u10" in ds and "v10" in ds:
         ds["wind_speed"] = (ds["u10"]**2 + ds["v10"]**2) ** 0.5
 
