5ca0555d19ef981be3b309476fe7ec1cfb0255a7
Ashish Raj <ashishraj@Ashishs-MacBook-Air-M3.local>
2026-03-15 07:30:19 +0530

    fix dataset loader

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 
 
-def load_dataset(path):   # removed @st.cache_data
+def load_dataset(path):
 
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -14,13 +14,11 @@ def load_dataset(path):   # removed @st.cache_data
         file_path = os.path.join(BASE_DIR, path)
         ds = xr.open_dataset(file_path)
 
-    # debugging
-    print(ds)
-    print(ds.coords)
-
+    # temperature conversion
     if "t2m" in ds:
         ds["t2m"] = ds["t2m"] - 273.15
 
+    # wind speed
     if "u10" in ds and "v10" in ds:
         ds["wind_speed"] = (ds["u10"]**2 + ds["v10"]**2) ** 0.5
 
